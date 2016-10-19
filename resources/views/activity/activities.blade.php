55ab54de6dc66d409a0855f1ae41066305cc19cd
congye6 <244053679@qq.com>
2016-10-19 20:45:13 +0800

    friends

--- resources/views/activity/activities.blade.php
+++ resources/views/activity/activities.blade.php
@@ -26,7 +26,49 @@
 
     @section('main-content')
         <div class="wrapper site-min-height">
+
             <div class="activity">
+                <!--table-->
+                <div class="row mt">
+
+                    <div class="col-md-offset-1 col-md-10 body">
+                        <div class="row ">
+                            <div class="col-lg-2">
+                                <div class="head">
+                                    <h4>会尽快的积分换房间大家</h4>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-2 centered">
+                                <p class="title"><b>参与人数</b></p>
+                                <h1><b>2</b></h1>
+                            </div>
+
+                            <div class="col-lg-2 centered">
+                                <p class="title"><b>运动类型</b></p>
+                                <img src="graphics/activity/sports.svg" alt="追踪器">
+                            </div>
+
+                            <div class="col-lg-4 centered">
+                                <p class="title"><b>离竞赛开始还有</b></p>
+                                <h1><b>1</b><small>天</small><b>3</b><small>小时</small></h1>
+
+                            </div>
+
+                            <div class="col-lg-2 centered">
+                                <p class="title"><b>奖金</b></p>
+                                <h1><b>100</b><small>金币</small></h1>
+                            </div>
+
+                        </div>
+
+
+
+                    </div>
+
+                </div>
+
+                <!--table-->
                 <div class="row mt">
 
                     <div class="col-md-offset-1 col-md-10 body">
@@ -40,6 +82,342 @@
                             <div class="col-lg-2 centered">
                                 <p class="title"><b>参与人数</b></p>
                                 <h1><b>2</b></h1>
+                            </div>
+
+                            <div class="col-lg-2 centered">
+                                <p class="title"><b>运动类型</b></p>
+                                <img src="graphics/activity/sports.svg" alt="追踪器">
+                            </div>
+
+                            <div class="col-lg-4 centered">
+                                <p class="title"><b>离竞赛开始还有</b></p>
+                                <h1><b>1</b><small>天</small><b>3</b><small>小时</small></h1>
+
+                            </div>
+
+                            <div class="col-lg-2 centered">
+                                <p class="title"><b>奖金</b></p>
+                                <h1><b>100</b><small>金币</small></h1>
+                            </div>
+
+                        </div>
+
+
+
+                    </div>
+
+                </div>
+
+                <!--table-->
+                <div class="row mt">
+
+                    <div class="col-md-offset-1 col-md-10 body">
+                        <div class="row ">
+                            <div class="col-lg-2">
+                                <div class="head">
+                                    <h4>hhhh</h4>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-2 centered">
+                                <p class="title"><b>参与人数</b></p>
+                                <h1><b>2</b></h1>
+                            </div>
+
+                            <div class="col-lg-2 centered">
+                                <p class="title"><b>运动类型</b></p>
+                                <img src="graphics/activity/sports.svg" alt="追踪器">
+                            </div>
+
+                            <div class="col-lg-4 centered">
+                                <p class="title"><b>离竞赛开始还有</b></p>
+                                <h1><b>1</b><small>天</small><b>3</b><small>小时</small></h1>
+
+                            </div>
+
+                            <div class="col-lg-2 centered">
+                                <p class="title"><b>奖金</b></p>
+                                <h1><b>100</b><small>金币</small></h1>
+                            </div>
+
+                        </div>
+
+
+
+                    </div>
+
+                </div>
+
+                <!--table-->
+                <div class="row mt">
+
+                    <div class="col-md-offset-1 col-md-10 body">
+                        <div class="row ">
+                            <div class="col-lg-2">
+                                <div class="head">
+                                    <h4>hhhh</h4>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-2 centered">
+                                <p class="title"><b>参与人数</b></p>
+                                <h1><b>2</b></h1>
+                            </div>
+
+                            <div class="col-lg-2 centered">
+                                <p class="title"><b>运动类型</b></p>
+                                <img src="graphics/activity/sports.svg" alt="追踪器">
+                            </div>
+
+                            <div class="col-lg-4 centered">
+                                <p class="title"><b>离竞赛开始还有</b></p>
+                                <h1><b>1</b><small>天</small><b>3</b><small>小时</small></h1>
+
+                            </div>
+
+                            <div class="col-lg-2 centered">
+                                <p class="title"><b>奖金</b></p>
+                                <h1><b>100</b><small>金币</small></h1>
+                            </div>
+
+                        </div>
+
+
+
+                    </div>
+
+                </div>
+
+                <!--table-->
+                <div class="row mt">
+
+                    <div class="col-md-offset-1 col-md-10 body">
+                        <div class="row ">
+                            <div class="col-lg-2">
+                                <div class="head">
+                                    <h4>hhhh</h4>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-2 centered">
+                                <p class="title"><b>参与人数</b></p>
+                                <h1><b>2</b></h1>
+                            </div>
+
+                            <div class="col-lg-2 centered">
+                                <p class="title"><b>运动类型</b></p>
+                                <img src="graphics/activity/sports.svg" alt="追踪器">
+                            </div>
+
+                            <div class="col-lg-4 centered">
+                                <p class="title"><b>离竞赛开始还有</b></p>
+                                <h1><b>1</b><small>天</small><b>3</b><small>小时</small></h1>
+
+                            </div>
+
+                            <div class="col-lg-2 centered">
+                                <p class="title"><b>奖金</b></p>
+                                <h1><b>100</b><small>金币</small></h1>
+                            </div>
+
+                        </div>
+
+
+
+                    </div>
+
+                </div>
+
+                <!--table-->
+                <div class="row mt">
+
+                    <div class="col-md-offset-1 col-md-10 body">
+                        <div class="row ">
+                            <div class="col-lg-2">
+                                <div class="head">
+                                    <h4>hhhh</h4>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-2 centered">
+                                <p class="title"><b>参与人数</b></p>
+                                <h1><b>2</b></h1>
+                            </div>
+
+                            <div class="col-lg-2 centered">
+                                <p class="title"><b>运动类型</b></p>
+                                <img src="graphics/activity/sports.svg" alt="追踪器">
+                            </div>
+
+                            <div class="col-lg-4 centered">
+                                <p class="title"><b>离竞赛开始还有</b></p>
+                                <h1><b>1</b><small>天</small><b>3</b><small>小时</small></h1>
+
+                            </div>
+
+                            <div class="col-lg-2 centered">
+                                <p class="title"><b>奖金</b></p>
+                                <h1><b>100</b><small>金币</small></h1>
+                            </div>
+
+                        </div>
+
+
+
+                    </div>
+
+                </div>
+
+                <!--table-->
+                <div class="row mt">
+
+                    <div class="col-md-offset-1 col-md-10 body">
+                        <div class="row ">
+                            <div class="col-lg-2">
+                                <div class="head">
+                                    <h4>hhhh</h4>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-2 centered">
+                                <p class="title"><b>参与人数</b></p>
+                                <h1><b>2</b></h1>
+                            </div>
+
+                            <div class="col-lg-2 centered">
+                                <p class="title"><b>运动类型</b></p>
+                                <img src="graphics/activity/sports.svg" alt="追踪器">
+                            </div>
+
+                            <div class="col-lg-4 centered">
+                                <p class="title"><b>离竞赛开始还有</b></p>
+                                <h1><b>1</b><small>天</small><b>3</b><small>小时</small></h1>
+
+                            </div>
+
+                            <div class="col-lg-2 centered">
+                                <p class="title"><b>奖金</b></p>
+                                <h1><b>100</b><small>金币</small></h1>
+                            </div>
+
+                        </div>
+
+
+
+                    </div>
+
+                </div>
+
+                <!--table-->
+                <div class="row mt">
+
+                    <div class="col-md-offset-1 col-md-10 body">
+                        <div class="row ">
+                            <div class="col-lg-2">
+                                <div class="head">
+                                    <h4>hhhh</h4>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-2 centered">
+                                <p class="title"><b>参与人数</b></p>
+                                <h1><b>2</b></h1>
+                            </div>
+
+                            <div class="col-lg-2 centered">
+                                <p class="title"><b>运动类型</b></p>
+                                <img src="graphics/activity/sports.svg" alt="追踪器">
+                            </div>
+
+                            <div class="col-lg-4 centered">
+                                <p class="title"><b>离竞赛开始还有</b></p>
+                                <h1><b>1</b><small>天</small><b>3</b><small>小时</small></h1>
+
+                            </div>
+
+                            <div class="col-lg-2 centered">
+                                <p class="title"><b>奖金</b></p>
+                                <h1><b>100</b><small>金币</small></h1>
+                            </div>
+
+                        </div>
+
+
+
+                    </div>
+
+                </div>
+
+                <!--table-->
+                <div class="row mt">
+
+                    <div class="col-md-offset-1 col-md-10 body">
+                        <div class="row ">
+                            <div class="col-lg-2">
+                                <div class="head">
+                                    <h4>hhhh</h4>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-2 centered">
+                                <p class="title"><b>参与人数</b></p>
+                                <h1><b>2</b></h1>
+                            </div>
+
+                            <div class="col-lg-2 centered">
+                                <p class="title"><b>运动类型</b></p>
+                                <img src="graphics/activity/sports.svg" alt="追踪器">
+                            </div>
+
+                            <div class="col-lg-4 centered">
+                                <p class="title"><b>离竞赛开始还有</b></p>
+                                <h1><b>1</b><small>天</small><b>3</b><small>小时</small></h1>
+
+                            </div>
+
+                            <div class="col-lg-2 centered">
+                                <p class="title"><b>奖金</b></p>
+                                <h1><b>100</b><small>金币</small></h1>
+                            </div>
+
+                        </div>
+
+
+
+                    </div>
+
+                </div>
+
+                <!--table-->
+                <div class="row mt">
+
+                    <div class="col-md-offset-1 col-md-10 body">
+                        <div class="row ">
+                            <div class="col-lg-2">
+                                <div class="head">
+                                    <h4>hhhh</h4>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-2 centered">
+                                <p class="title"><b>参与人数</b></p>
+                                <h1><b>2</b></h1>
+                            </div>
+
+                            <div class="col-lg-2 centered">
+                                <p class="title"><b>运动类型</b></p>
+                                <img src="graphics/activity/sports.svg" alt="追踪器">
+                            </div>
+
+                            <div class="col-lg-4 centered">
+                                <p class="title"><b>离竞赛开始还有</b></p>
+                                <h1><b>1</b><small>天</small><b>3</b><small>小时</small></h1>
+
+                            </div>
+
+                            <div class="col-lg-2 centered">
+                                <p class="title"><b>奖金</b></p>
+                                <h1><b>100</b><small>金币</small></h1>
                             </div>
 
                         </div>
--- resources/views/friends/followed.blade.php
+++ resources/views/friends/followed.blade.php
@@ -3,3 +3,21 @@
     @section('select-page')
         setSelectMenu(2,7);
     @endsection
+
+    @section('title')
+        我的关注
+    @endsection
+
+    @section('go-top')
+        /followedFriends#
+    @endsection
+
+    @section('css')
+        @@parent
+
+    @endsection
+
+    @section('js')
+        @@parent
+
+    @endsection
