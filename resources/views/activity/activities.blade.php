c48cc8e257f4c6b99ab7e1c39fc1ac9e25709f56
congye6 <244053679@qq.com>
2016-10-19 18:54:35 +0800

    activity

--- resources/views/activity/activities.blade.php
+++ resources/views/activity/activities.blade.php
@@ -3,3 +3,54 @@
     @section('select-page')
         setSelectMenu(1,5);
     @endsection
+
+    @section('title')
+        竞技场
+    @endsection
+
+    @section('go-top')
+        /activities#
+    @endsection
+
+    @section('css')
+        @@parent
+        <link rel="stylesheet" href="css/activity/activities.css">
+    @endsection
+
+    @section('js')
+        @@parent
+
+    @endsection
+
+
+
+    @section('main-content')
+        <div class="wrapper site-min-height">
+            <div class="activity">
+                <div class="row mt">
+
+                    <div class="col-md-offset-1 col-md-10 body">
+                        <div class="row ">
+                            <div class="col-lg-2">
+                                <div class="head">
+                                    <h4>hhhh</h4>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-2 centered">
+                                <p class="title"><b>参与人数</b></p>
+                                <h1><b>2</b></h1>
+                            </div>
+
+                        </div>
+
+
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    @endsection
--- resources/views/activity/publish.blade.php
+++ resources/views/activity/publish.blade.php
@@ -17,6 +17,12 @@
 
     @section('js')
         @@parent
+        <script src="js/activity/autosize.js"></script>
+        <script>
+
+            autosize(document.querySelectorAll('textarea'));
+
+        </script>
     @endsection
 
 
@@ -24,11 +30,116 @@
     @section('main-content')
         <div class="wrapper site-min-height">
             <div class="row mt">
-                <div class="col-md-12">
-                    <textarea style="width:500px"></textarea>
+                <div class="col-md-2">
+                    <h2>竞赛介绍</h2>
+                </div>
+                <div class="col-md-8 mt">
+                    <textarea style="max-height: 300px; min-height:150px;min-width: 500px;resize: none;"></textarea>
+                </div>
+            </div>
 
+            <div class="row mt">
+                <div class="col-md-2">
+                    <h2>时间</h2>
+                </div>
+
+                <!--select day-->
+                <div class="col-md-2 mt">
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-theme">今天</button>
+                        <button type="button" class="btn btn-theme dropdown-toggle" data-toggle="dropdown">
+                            <span class="caret"></span>
+                            <span class="sr-only">Toggle Dropdown</span>
+                        </button>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="#">Action</a></li>
+                            <li><a href="#">Another action</a></li>
+                            <li><a href="#">Something else here</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">Separated link</a></li>
+                        </ul>
+                    </div>
 
                 </div>
+                <div class="col-md-1">
+
+                    <h2>--</h2>
+                </div>
+
+
+                <div class="col-md-2 mt">
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-theme">今天</button>
+                        <button type="button" class="btn btn-theme dropdown-toggle" data-toggle="dropdown">
+                            <span class="caret"></span>
+                            <span class="sr-only">Toggle Dropdown</span>
+                        </button>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="#">Action</a></li>
+                            <li><a href="#">Another action</a></li>
+                            <li><a href="#">Something else here</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">Separated link</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+
+            </div>
+
+            <div class="row mt">
+                <div class="col-md-2">
+                    <h2>奖金</h2>
+                </div>
+
+                <div class="col-md-2 mt">
+                    <div class="form-group">
+                        <input type="email" class="form-control" id="exampleInputEmail2">
+                    </div>
+                </div>
+
+                <div class="col-md-2">
+                    <h2>金币</h2>
+                </div>
+
+            </div>
+
+            <div class="row mt">
+                <div class="col-md-2">
+                    <h2>运动类型</h2>
+                </div>
+
+                <!--select day-->
+                <div class="col-md-2 mt">
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-theme">追踪器</button>
+                        <button type="button" class="btn btn-theme dropdown-toggle" data-toggle="dropdown">
+                            <span class="caret"></span>
+                            <span class="sr-only">Toggle Dropdown</span>
+                        </button>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="#">Action</a></li>
+                            <li><a href="#">Another action</a></li>
+                            <li><a href="#">Something else here</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">Separated link</a></li>
+                        </ul>
+                    </div>
+
+                </div>
+
+
+
+            </div>
+
+            <div class="row mt">
+                <div class="col-md-1 col-md-offset-1 mt">
+                    <button type="button" class="btn btn-theme btn-round">发布竞赛</button>
+                </div>
+                <div class="col-md-1 mt">
+                    <button type="button" class="btn btn-theme btn-round">取消</button>
+                </div>
+
             </div>
 
         </div>
